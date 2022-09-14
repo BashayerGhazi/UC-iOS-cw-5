b8609d2bfcbd692787f679588dede8a99a81ec04
BashayerGhazi <112524443+BashayerGhazi@users.noreply.github.com>
2022-09-14 19:08:01 +0300

    done

---
+++ emojis/emojis/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  emojis
+//
+//  Created by Shahad on 14/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var emojis = ["🥰"," 🥸","🤪","🤯", "😤","🥶","😇","🥱", "🤧","🤬"]
+    @State var W = ""
+    var body: some View {
+        VStack {
+            
+            Text("اختار الايموجي اللي يعبد عنك اليوم").font(.largeTitle).multilineTextAlignment(.center).padding()
+            Text(W).font(.system(size: 90))
+            
+            
+            ScrollView(.horizontal){
+            
+                HStack{
+                    ForEach(emojis, id: \.self ) { emoji in
+                        Text(emoji)
+                            .font(.system(size: 50))
+                            
+                            .onTapGesture {
+                                W = emoji
+                            }
+                            
+                        
+                    }
+                }
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ emojis/emojis/emojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojisApp.swift
+//  emojis
+//
+//  Created by Shahad on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct emojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
