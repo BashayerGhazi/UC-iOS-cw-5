c7033cc8934b947d30682d89e0f0afeb9b351aa0
BashayerGhazi <112524443+BashayerGhazi@users.noreply.github.com>
2022-09-15 03:53:49 +0300

    done

--- BMI calculator/BMI calculator/ContentView.swift
+++ BMI calculator/BMI calculator/ContentView.swift
@@ -10,35 +10,63 @@ import SwiftUI
 struct ContentView: View {
     @State var weight = ""
     @State var hight = ""
+    @State  var result = 0.0
+    @State var Status = ""
+    //@State var res: Double = 0.0
+    //@State var resu =  ( (Double(weight) ?? 0.0) /
+    //( (Double(hight) ?? 0.0 ) * (Double(hight) ?? 0.0) )
+   
     var body: some View {
         VStack {
             Image("weight")
             Text("ادخل الطول والوزن").font(.largeTitle)
             TextField("m", text: $hight).font(.largeTitle).padding()
             TextField("kg", text: $weight).font(.largeTitle).padding()
-            Button(action:{
-                
-            } ){
-                ZStack{
             
-                    Image(systemName: "square.fill")
-                        Color.orange.frame(width: 200, height: 60)
-                    Text("احسب").font(.largeTitle)
-                }
+           // let  result = ( (Double(weight) ?? 0.0) / ( (Double(hight) ?? 0.0 ) * (Double(hight) ?? 0.0) ) )
+                                     
+            Button(action:{
+                CALC(weight: Double(weight) ?? 0.0, hight: Double(hight) ?? 0.0 )
+                } ){
+                ZStack{
+                    Text("احسب").font(.largeTitle).foregroundColor(.white)
+                        .frame(width: 200, height: 60).background(.black).cornerRadius(10)
+                      }
                 
+                }
+                .padding()
+            Text("BMI = \(result) ")
+            Text("الحالة \(Status)")
+                }
+        
+
             }
-            .padding()
-        }
+
+    func CALC(weight: Double, hight: Double) {
+
+         result = ( Double(weight)  / ( Double(hight) * Double(hight) ) )
+
+          
+    if result <= 20.0 {
+        Status = "ضعيف"
+                        
+            } else if result <= 25.0 {
+                Status = "جيد"
+            } else {
+                Status = "سمين"
+                    }
+                    
+                    
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+    }
+                                    
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+ContentView()
     }
 }
-//func CALC(weight: String ) -> String {
- //   return
-//    Text("good")
-//}
+    
+                                     
 
+                                     
